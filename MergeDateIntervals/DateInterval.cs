749877836445834fe9b96e126ec058bbccce3815
Kakaranish <stasiekgruz@gmail.com>
2021-07-05 21:48:09 +0200

    + merging date intervals

---
+++ MergeDateIntervals/DateInterval.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MergeDateIntervals
+{
+    public class DateInterval
+    {
+        public DateTime Start { get; }
+        public DateTime End { get; }
+
+        public DateInterval(DateTime start, DateTime end)
+        {
+            Start = start.Date;
+            End = end.Date;
+
+            if (Start.Subtract(End).Ticks > 0)
+            {
+                throw new ArgumentException($"{nameof(Start)} must be before {nameof(End)}");
+            }
+        }
+
+        public bool IsAdjacent(DateInterval other)
+        {
+            return End.AddDays(1) == other.Start || Start.AddDays(-1) == other.End;
+        }
+
+        public bool Overlap(DateInterval other)
+        {
+            return (End >= other.Start) ||
+                   (Start >= other.End) ||
+                   (Start <= other.Start && End >= other.End) ||
+                   (other.Start <= Start && other.End >= End);
+        }
+
+        public override string ToString()
+        {
+            return $"[{Start.ToShortDateString()} - {End.ToShortDateString()}]";
+        }
+    }
+}
---
+++ MergeDateIntervals/DateUtility.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MergeDateIntervals
+{
+    public class DateUtility
+    {
+        public IEnumerable<DateInterval> MergeDateIntervals(IList<DateInterval> dateIntervals)
+        {
+            if (dateIntervals == null) throw new ArgumentNullException(nameof(dateIntervals));
+            if (dateIntervals.Count < 2) return dateIntervals;
+
+            var sortedDateIntervals = dateIntervals.OrderBy(x => x.Start).ToList();
+            var mergedDateIntervals = new Stack<DateInterval>();
+            mergedDateIntervals.Push(sortedDateIntervals[0]);
+
+            foreach (var dateInterval in sortedDateIntervals.Skip(1))
+            {
+                var lastMergedInterval = mergedDateIntervals.Peek();
+                if (!TryMergeIntervals(lastMergedInterval, dateInterval, out var mergedDateInterval))
+                {
+                    mergedDateIntervals.Push(dateInterval);
+                    continue;
+                }
+
+                mergedDateIntervals.Pop();
+                mergedDateIntervals.Push(mergedDateInterval);
+            }
+
+            return mergedDateIntervals.Reverse().ToList();
+        }
+
+        private static bool TryMergeIntervals(DateInterval interval1, DateInterval interval2, out DateInterval result)
+        {
+            if (!interval1.IsAdjacent(interval2) && !interval1.Overlap(interval2))
+            {
+                result = null;
+                return false;
+            }
+
+            var startDate = interval1.Start < interval2.Start ? interval1.Start : interval2.End;
+            var endDate = interval1.End > interval2.End ? interval1.End : interval2.End;
+
+            result = new DateInterval(startDate, endDate);
+            return true;
+        }
+    }
+}
